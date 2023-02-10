b3a14e059a7fc2fe484f7a84135513da5338a9ba
unknown <lazycode686@gmail.com>
2023-02-10 06:52:51 -0800

    version 1.1

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
 
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
+
+
 window.onload = () =>{
 
-    if(window.scrollY > 80){
-      document.querySelector('.header .header-2').classList.add('active');
-    }else{
-      document.querySelector('.header .header-2').classList.remove('active');
-    }
-  
-    // fadeOut();
-  
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
   }
+
+  // fadeOut();
+  
+}
   
 
 function fadeOut(){
